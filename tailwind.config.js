bc767762cd49d05aa7c7583a27c2ff59d6aecf0d
Jeya rosini <jeya.rosini@gmail.com>
2023-10-03 14:36:46 +0530

    add the background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {
-      backgroundImage: {
-        'hero-pattern': "url('/Images/background.jpg')",
-      }
-    },
+    extend: {},
   },
   plugins: [],
   }
